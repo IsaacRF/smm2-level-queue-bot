151ef2f19b2dfec5c1690e37a64ccc5a87f02cb9
Isaac R.F <isaacrf239@gmail.com>
2020-04-27 23:33:51 +0200

    Creates overlay basic structure. Adds socket connection methods

---
+++ overlay/client.js
@@ -0,0 +1,50 @@
+if( window.WebSocket ){
+    //---------------------------------
+    //  Variables
+    //---------------------------------
+    socket = null;
+    var reconnectIntervalMs = 10000;
+
+    function Connect() {
+        socket.onopen = function()
+        {
+            // Format your Authentication Information
+            var auth = {
+                author: "IsaacRF239",
+                website: "https://isaacrf.com",
+                api_key: API_Key,
+                events: [
+                    "EVENT_SMM2QS_LEVEL_UPDATE",
+                ]
+            }
+            //Send your Data to the server
+            socket.send(JSON.stringify(auth));
+        };
+
+        socket.onerror = function(error)
+        {
+            console.log("Error: " + error);
+        }
+
+        socket.onmessage = function (message)
+        {
+            var jsonObject = JSON.parse(message.data);
+
+            if(jsonObject.event == "EVENT_SMM2QS_LEVEL_UPDATE")
+            {
+                //SAMPLE
+                //var MySet = JSON.parse(jsonObject.data);
+                //document.getElementById("myimg").src=MySet.link;
+            }
+        }
+
+        socket.onclose = function ()
+        {
+            //  Connection has been closed by you or the server
+            console.log("Connection Closed!");
+            setTimeout(Connect, reconnectIntervalMs);
+        }
+    }
+
+    Connect();
+}
