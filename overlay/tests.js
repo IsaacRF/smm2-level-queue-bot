9dd7c51778eecd8b2c822c7acbd1cea12e01f59f
Isaac R.F <isaacrf239@gmail.com>
2020-05-05 18:35:27 +0200

    Changes tests behaviour to better represent actual bot levels flow

--- overlay/tests.js
+++ overlay/tests.js
@@ -1,7 +1,10 @@
 var testUsers = ['clickandslash', 'ggpalma', 'sirteclas', 'tomatefey', 'zeroretr0', 'steyb'];
 var testWins = 0;
 var testSkips = 0;
+var testLevels = [];
+var testCurrentLevelPosition = 0;
 var testsEnabled = true;
+
 
 /**
  * Return a random user from testUsers list
@@ -13,7 +16,7 @@ function testGetRandomUserName() {
 /**
  * Returns a randomly generated SMM2 legal level code
  */
-function testCreateLevelCode() {
+function testGenerateRandomLevelCode() {
     var result           = '';
     var characters       = 'ABCDEFGHJKLMNPQRSTUVWXYZ0123456789';
     var charactersLength = characters.length;
@@ -28,21 +31,72 @@ function testCreateLevelCode() {
 }
 
 /**
- * Simulates a level win
- * @param {int} levelsReturned 0 for empty - 1 for current level only - 2 for both current and next levels
+ * Test initialization
+ * @param {int} levelsNumber Number of levels to generate
  */
-function testWinLevel(levelsReturned) {
-    testWins += 1;
-    testUpdateUIRandom(levelsReturned);
+function testInit(levelsNumber) {
+    testLevels = [];
+    testAddLevel(levelsNumber);
+    testUpdateUI();
+}
+
+/**
+ * Simulates a level win
+ */
+function testWinLevel() {
+    if (testLevels.length > 0) {
+        testWins += 1;
+        testLevels.shift();
+        testUpdateUI();
+        console.log(testLevels.length + " niveles más en la lista");
+    } else {
+        console.log("No hay niveles, no se han modificado los contadores ni la UI");
+    }
 }
 
 /**
  * Simulates a level skip
- * @param {int} levelsReturned 0 for empty - 1 for current level only - 2 for both current and next levels
  */
-function testSkipLevel(levelsReturned) {
-    testSkips += 1;
-    testUpdateUIRandom(levelsReturned);
+function testSkipLevel() {
+    if (testLevels.length > 0) {
+        testSkips += 1;
+        testLevels.shift();
+        testUpdateUI();
+        console.log(testLevels.length + " niveles más en la lista");
+    } else {
+        console.log("No hay niveles, no se han modificado los contadores ni la UI");
+    }
+}
+
+/**
+ * Generates specified number of random levels and adds them to list. Updates UI only
+ * if there are 1 or less levels remaining in list.
+ * @param {int} levelsNumber Number of levels to add to list. Default is 1.
+ */
+function testAddLevel(levelsNumber=1) {
+    var isUpdateUIRequired = (testLevels.length <= 1);
+
+    for (let i = 0; i < levelsNumber; i++) {
+        testLevels.push(new TestLevel());
+    }
+
+    if (isUpdateUIRequired) {
+        testUpdateUI();
+    }
+}
+
+/**
+ * Updates UI with next levels in list
+ */
+function testUpdateUI() {
+    var testJsonData = '{"currentLevelCode": "' + (testLevels.length >= 1 ? testLevels[0].code : "") + '", ' +
+                        '"currentLevelUser": "' + (testLevels.length >= 1 ? testLevels[0].user : "") + '", ' +
+                        '"nextLevelCode": "' + (testLevels.length >= 2 ? testLevels[1].code : "") + '", ' +
+                        '"nextLevelUser": "' + (testLevels.length >= 2 ? testLevels[1].user : "") + '", ' +
+                        '"wins": ' + testWins +', ' +
+                        '"skips": ' + testSkips + '}';
+
+    updateUI(testJsonData);
 }
 
 /**
@@ -50,12 +104,19 @@ function testSkipLevel(levelsReturned) {
  * @param {int} levelsReturned 0 for empty - 1 for current level only - 2 for both current and next levels
  */
 function testUpdateUIRandom(levelsReturned) {
-    var testJsonData = '{"currentLevelCode": "' + ((levelsReturned >= 1) ? testCreateLevelCode() : "") + '", ' +
+    var testJsonData = '{"currentLevelCode": "' + ((levelsReturned >= 1) ? testGenerateRandomLevelCode() : "") + '", ' +
                         '"currentLevelUser": "' + ((levelsReturned >= 1) ? testGetRandomUserName() : "") + '", ' +
-                        '"nextLevelCode": "' + ((levelsReturned >= 2) ? testCreateLevelCode() : "") + '", ' +
+                        '"nextLevelCode": "' + ((levelsReturned >= 2) ? testGenerateRandomLevelCode() : "") + '", ' +
                         '"nextLevelUser": "' + ((levelsReturned >= 2) ? testGetRandomUserName() : "") + '", ' +
                         '"wins": ' + testWins +', ' +
                         '"skips": ' + testSkips + '}';
 
     updateUI(testJsonData);
+}
+
+class TestLevel {
+    constructor() {
+        this.code = testGenerateRandomLevelCode();
+        this.user = testGetRandomUserName();
+    }
 }
